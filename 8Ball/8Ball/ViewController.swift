a28445108d6579480b8f92c7a2d1ee6396bc360f
STNikolaev <a.o.nikolaev@icloud.com>
2022-02-06 14:33:04 +0300

    initial commit

---
+++ 8Ball/8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8Ball
+//
+//  Created by Alexandr Nikolaev on 6.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
