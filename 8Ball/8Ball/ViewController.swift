3b75a56a8ce7171fb83fb0e6769bc8d684afc5f8
STNikolaev <a.o.nikolaev@icloud.com>
2022-02-06 15:55:05 +0300

    Ready without constraints

--- 8Ball/8Ball/ViewController.swift
+++ 8Ball/8Ball/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImageView: UIImageView!
+    @IBOutlet weak var askButtonView: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        askButtonView.layer.cornerRadius = askButtonView.frame.height / 8
     }
+    
+    
 
-
+    @IBAction func AskButtonAction(_ sender: UIButton) {
+        let answersArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
+        ballImageView.image = answersArray.randomElement()
+    }
+    
 }
 
